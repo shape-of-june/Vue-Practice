9d57095b0aeb16ebf03051f546322c0b0946c81c
shape-of-june <kyle14916@naver.com>
2024-06-15 12:49:24 +0900

    Sㅣㅑㅎ
    Changes to be committed:
    	modified:   src/store/index.js

--- src/store/index.js
+++ src/store/index.js
@@ -7,23 +7,23 @@ const store = createStore({
       // 로그인 정보 (전역에서 사용)
       isLoggedIn: false,
 
-      // 현재 누른 책 정보 (EditorView에서 사용) 
-      // => 최적화시 BookView에서만 API로 호출하여 받아와서 사용 (전역변수를 줄임)
-      // vs 사용자가 로그인 시 모든 책 정보를 받아오기? (이건 느릴 듯)
+      // <이슈> EditorView로 옮기기
+      // 현재
       bookName: '',
       bookContent: [],
 
-      // 로그인한 사용자가 가진 모든 책의 정보 (BookView, EditorView에서 사용)
+      // <이슈> 이것도 로그인 정보에 따라 동적으로 BookView로 옮기기
       books: ['책1', '책2', '책3', '책4'],
       finishedBooks: [],
       
-      // question1과 buttonList는 최적화시 RecommendView1에서만 하드코딩하여 사용 (전역변수를 줄임)
+      // <이슈> recommendView1로 옮기기
       question1: '이야기 하고 싶은 분야를 알려주세요! 3번의 단계만 거치면 글감을 찾을 수 있어요!',
       buttonList: [
         '여행', '사진', '영화', '책', '글쓰기',
         '음악', '요리', '육아', '스타트업', '건강',
         '운동', '멘탈', '인문학', '사랑', '인생',
       ],
+
       // if) answer1,2,3이 모두 editor에 영향을 주는 변수라면 answer들을 저장해야 함
       // else) answer1,2들이 editor에 영향을 주지 않는다면 answer들을 저장하지 않고 각 뷰에서
       // 호출하여 얻은 다음 질문들만을 저장하고 최종 글감을 writeAbles에 저장
@@ -34,6 +34,7 @@ const store = createStore({
 
       question3List: [],
       answer3List: [],
+      // 
       writeAbles: [],
     }
   },
